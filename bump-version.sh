d07721095217fa4e08d275df2c96e43ffe93d6a8
houdini91 <mdstrauss91@gmail.com>
2024-01-31 13:17:20 +0200

    add bump version

--- bump-version.sh
+++ bump-version.sh
@@ -5,9 +5,10 @@ version=$1
 git checkout dev
 git pull origin dev
 git branch -D  prep_release/$version || true
-git checkout -b prep_release/$version
+git checkout -b prep_release/$version || git checkout prep_release/$version || true
 sed -i "s/dev-latest/$version/g" action.yml
 git add action.yml || true
 git commit -m "bump version" || true
+git fetch origin master
 git merge --no-edit -s ours origin/master || true
 git push -f origin prep_release/$version || true
