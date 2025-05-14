d7eda8d64e815959cef9fb3069239a6122039e71
houdini91 <mdstrauss91@gmail.com>
2025-05-14 10:18:31 +0300

    fix bump version

--- bump-version.sh
+++ bump-version.sh
@@ -6,7 +6,7 @@ git checkout dev
 git pull origin dev
 git branch -D  prep_release/$version || true
 git checkout -b prep_release/$version || git checkout prep_release/$version || true
-sed -i "s/dev-latest/$version/g" action.yml
+sed -i -E "s|docker://[^\"'[:space:]]*/valint:[^\"'[:space:]]+|docker://scribesecurity/valint:${version}|g" action.yml
 git add action.yml || true
 git commit -m "bump version" || true
 git fetch origin master
